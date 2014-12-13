8ab668880deb1ee96fef2c14ca59bfafcd833641
Vijesh <vijeshjain@users.noreply.github.com>
2014-12-12 16:42:12 -0800

    Delete auth.js

--- config/auth.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-
-	'facebookAuth' : {
-        'clientID'      : '241283665919411', // your App ID
-        'clientSecret'  : 'a11ac347e90b0ee86358697b10756267', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
-    },
-};
